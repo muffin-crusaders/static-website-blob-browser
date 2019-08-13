c554718844c1cb04a6398535c218a2f91cd602a7
Aleksuei Riabtsev <aleksuei.riabtsev@ec.gc.ca>
2019-08-13 13:10:21 -0400

    chore: pre-fetch blob lists

--- src/index.js
+++ src/index.js
@@ -17,6 +17,9 @@ const account = 'rampdemoblobstorage008';
 const container = '$web';
 
 class App extends React.Component {
+    prefetchedBlobs = {};
+    tableState = {};
+
     constructor() {
         super();
         this.state = {
@@ -26,12 +29,39 @@ class App extends React.Component {
             loading: true,
             prefix: ''
         };
-        this.fetchData = this.listBlobs.bind(this);
+
+        this.onTableMount = state => {
+            // NOTE: I couldn't figure out a better way to pass table state to the `fetchBlobs` when a link in the table cell is clicked
+            // TODO: find a way around storing state explicitly
+            this.tableState = state;
+
+            this.listBlobs(this.tableState, this.prefix);
+        };
+
+        // catch back/forward browser button clicks
+        window.onpopstate = event => {
+            this.listBlobs(this.tableState, this.prefix);
+        };
     }
 
-    listBlobs(state, instance) {
-        // this lists Blobs in pages defined in state.pageSize
-        this.setState({ loading: true });
+    get prefix() {
+        // Fetch the prefix in the query params to browse into folders
+        const urlParams = new URLSearchParams(window.location.search);
+
+        let prefix = window.location.pathname !== '/' ? window.location.pathname.slice(1) : urlParams.get('prefix');
+        if (prefix !== null && prefix.length > 0 && prefix.slice(-1) !== '/') {
+            prefix += '/';
+        }
+
+        window.history.pushState(null, '', window.location.origin);
+
+        return prefix;
+    }
+
+    fetchBlobs(state, prefix = '') {
+        if (this.prefetchedBlobs[prefix]) {
+            return this.prefetchedBlobs[prefix];
+        }
 
         // Use AnonymousCredential since $web container is made a 'public container'
         // and does not require authorization
@@ -45,73 +75,98 @@ class App extends React.Component {
         const containerName = container; //+ `?st=2018-11-06T06%3A15%3A24Z&se=2019-11-07T06%3A15%3A00Z&sp=rl&sv=2018-03-28&sr=c&sig=4vCT7aInDWRiypkuYlezN8dos0K2h2DvQ0pnNkMJSFs%3D`;
         const containerURL = ContainerURL.fromServiceURL(serviceURL, containerName);
 
-        // Fetch the prefix in the query params to browse into folders
-        const urlParams = new URLSearchParams(window.location.search);
-        
-        let prefix = window.location.pathname !== '/' ? window.location.pathname.slice(1) : urlParams.get('prefix');
-        if (prefix !== null && prefix.length > 0 && prefix.slice(-1) !== '/') {
-            prefix += '/';
+        // List objects from Blob storage using the prefix
+        // Delimiter for virtual directories is a forward slash '/' here
+        const fetchPromise = containerURL.listBlobHierarchySegment(Aborter.none, '/', undefined, {
+            maxresults: state.pageSize,
+            prefix: prefix
+        });
+
+        this.prefetchedBlobs[prefix] = fetchPromise;
+
+        return fetchPromise;
+    }
+
+    listBlobs(state, prefix, pushUrl = `?prefix=${prefix}`) {
+        // this lists Blobs in pages defined in state.pageSize
+        this.setState({ loading: true });
+
+        // Push the new state into the url
+        // if (pushUrl) {
+        window.history.pushState(null, '', pushUrl);
+        // }
+
+        // fetch blob lists referenced by breadcrumbs
+        const prefixParts = (prefix || '')
+            .split('/')
+            .filter(a => a)
+            .slice(0, -1);
+
+        // load blob lists from the breadcrumb folders
+        if (prefixParts.length > 0) {
+            prefixParts.map((step, index) => this.fetchBlobs(state, prefixParts.slice(0, index + 1).join('/')));
         }
 
         // List objects from Blob storage using the prefix
         // Delimiter for virtual directories is a forward slash '/' here
-        containerURL
-            .listBlobHierarchySegment(Aborter.none, '/', state.markers[state.page], {
-                maxresults: state.pageSize,
-                prefix: prefix
-            })
-            .then(res => {
-                // Store the nextMarker in an array for prev/next buttons only if there are more blobs to show
-                const markers = state.markers.slice();
-                var totalPages = state.page + 1;
-                if (res.nextMarker) {
-                    markers[state.page + 1] = res.nextMarker;
-                    totalPages++;
-                }
+        this.fetchBlobs(state, prefix).then(res => {
+            // Store the nextMarker in an array for prev/next buttons only if there are more blobs to show
+            const markers = state.markers.slice();
+            var totalPages = state.page + 1;
+            if (res.nextMarker) {
+                markers[state.page + 1] = res.nextMarker;
+                totalPages++;
+            }
 
-                // Combine the found virtual directories and files
-                // move folders to the top of the list
-                res.segment.blobItems = [...res.segment.blobPrefixes, ...res.segment.blobItems];
+            // Combine the found virtual directories and files
+            // move folders to the top of the list
+            const allBlobItems = [...res.segment.blobPrefixes, ...res.segment.blobItems];
 
-                // This is to sort rows, and handles blobName, contentLength and lastModified time
-                const sortedData = _.orderBy(
-                    res.segment.blobItems.filter(bi => bi.name !== 'view/'), // the source of this viewer is in the 'view/' folder, so we exclude it
-                    state.sorted.map(sort => {
-                        return row => {
-                            if (row[sort.id] === null) {
+            // load blob list from all the visible folder
+            res.segment.blobPrefixes.forEach(blobPrefix => this.fetchBlobs(state, blobPrefix.name));
+            console.log(this.prefetchedBlobs);
+
+            // This is to sort rows, and handles blobName, contentLength and lastModified time
+            const sortedData = _.orderBy(
+                allBlobItems.filter(bi => bi.name !== 'view/'), // the source of this viewer is in the 'view/' folder, so we exclude it
+                state.sorted.map(sort => {
+                    return row => {
+                        if (row[sort.id] === null) {
+                            return -Infinity;
+                        } // TODO: following is a workaround to special case contentLength and lastModified
+                        else if (row[sort.id] === undefined) {
+                            if (row.properties === undefined) {
                                 return -Infinity;
-                            } // TODO: following is a workaround to special case contentLength and lastModified
-                            else if (row[sort.id] === undefined) {
-                                if (row.properties === undefined) {
-                                    return -Infinity;
-                                } else {
-                                    return row.properties[sort.id];
-                                }
+                            } else {
+                                return row.properties[sort.id];
                             }
-                            return typeof row[sort.id] === 'string' ? row[sort.id].toLowerCase() : row[sort.id];
-                        };
-                    }),
-                    state.sorted.map(d => (d.desc ? 'desc' : 'asc'))
-                );
+                        }
+                        return typeof row[sort.id] === 'string' ? row[sort.id].toLowerCase() : row[sort.id];
+                    };
+                }),
+                state.sorted.map(d => (d.desc ? 'desc' : 'asc'))
+            );
 
-                // Store the state
-                this.setState({
-                    data: sortedData,
-                    pages: totalPages,
-                    markers: markers,
-                    loading: false,
-                    prefix: prefix
-                });
+            // Store the state
+            this.setState({
+                data: sortedData,
+                pages: totalPages,
+                markers: markers,
+                loading: false,
+                prefix: prefix
             });
+        });
     }
 
     // Custom links for various scenarios (handles blobs, directories and go back link)
     renderLink(blobName) {
-        var link;
-
+        let link;
         const { prefix } = this.state;
+        let folderName = null;
+        let isFolder = true;
 
         if (blobName === '../') {
+            // level up
             // link = '/';
 
             const prePrefix = prefix
@@ -120,15 +175,34 @@ class App extends React.Component {
                 .join('/');
 
             link = prePrefix === '' ? '/' : '?prefix=' + prePrefix;
+            folderName = prePrefix === '' ? null : prePrefix + '/';
         } else if (blobName.slice(-1) === '/') {
+            // folder
             link = '?prefix=' + blobName;
+            folderName = blobName;
             blobName = blobName.split('/').slice(-2, -1) + '/';
         } else {
+            // file
+            isFolder = false;
             link = '/' + blobName;
             blobName = blobName.split('/').pop();
         }
 
-        return <a href={window.location.origin + link}>{blobName}</a>;
+        return (
+            <a
+                href={window.location.origin + link}
+                onClick={e => {
+                    if (!isFolder) {
+                        return;
+                    }
+
+                    e.preventDefault();
+                    this.listBlobs(this.tableState, folderName, link);
+                }}
+            >
+                {blobName}
+            </a>
+        );
     }
 
     render() {
@@ -142,7 +216,7 @@ class App extends React.Component {
 
         return (
             <div>
-                <Breadcrumbs path={prefix} />
+                <Breadcrumbs path={prefix} onClick={(prefix, link) => this.listBlobs(this.tableState, prefix, link)} />
 
                 <ReactTable
                     columns={[
@@ -150,7 +224,7 @@ class App extends React.Component {
                             Header: 'Blob Name',
                             id: 'name',
                             accessor: 'name',
-                            Cell: row => this.renderLink(row.value)
+                            Cell: ({ value }) => this.renderLink(value)
                         },
                         {
                             Header: 'Last Modified',
@@ -161,7 +235,9 @@ class App extends React.Component {
                                 }
                             },
                             Cell: row =>
-                                row.value ? <span title={row.value.format('YYYY-MM-DD hh:mm:ss')}>{row.value.fromNow()}</span> : null,
+                                row.value ? (
+                                    <span title={row.value.format('YYYY-MM-DD hh:mm:ss')}>{row.value.fromNow()}</span>
+                                ) : null,
                             maxWidth: 400
                         },
                         {
@@ -181,7 +257,7 @@ class App extends React.Component {
                     minRows={0}
                     markers={markers}
                     loading={loading}
-                    onFetchData={this.fetchData}
+                    onFetchData={this.onTableMount}
                     showPagination={pages !== 1}
                     pageSizeOptions={[5000]}
                     defaultPageSize={5000}
@@ -200,7 +276,16 @@ class Breadcrumbs extends React.Component {
         // add link to the root
         const crumbs = [
             <React.Fragment key="root">
-                <a href={window.location.origin}>root</a>
+                <a
+                    href={window.location.origin}
+                    onClick={e => {
+                        e.preventDefault();
+                        // this will load the root blobs
+                        this.props.onClick(null, '/');
+                    }}
+                >
+                    root
+                </a>
                 <span> / </span>
             </React.Fragment>
         ];
@@ -208,12 +293,24 @@ class Breadcrumbs extends React.Component {
         // create links for all breadcrumbs save the last one
         if (pathSteps.length > 0) {
             crumbs.push(
-                ...pathSteps.slice(0, -1).map((step, index) => (
-                    <React.Fragment key={step}>
-                        <a href={`${window.location.origin}?prefix=${pathSteps.slice(0, index + 1).join('/')}`}>{step}</a>
-                        <span> / </span>
-                    </React.Fragment>
-                ))
+                ...pathSteps.slice(0, -1).map((step, index) => {
+                    const prefix = `${pathSteps.slice(0, index + 1).join('/')}/`;
+                    const link = `?prefix=${prefix}`;
+                    return (
+                        <React.Fragment key={step}>
+                            <a
+                                href={`${window.location.origin}${link}`}
+                                onClick={e => {
+                                    e.preventDefault();
+                                    this.props.onClick(prefix, link);
+                                }}
+                            >
+                                {step}
+                            </a>
+                            <span> / </span>
+                        </React.Fragment>
+                    );
+                })
             );
 
             // add the last breadcrumb as a simple span
